b023ae9e89ebd1658ce6494af8cfe58456e725f5
Zishan Siddique <zishansiddique80@gmail.com>
2024-06-20 14:27:18 +0545

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="flask_db",
+    version="0.0.01",
+    description="flask server with postgresql connection using psycopg2",
+    author="Zishan Siddique",
+    install_requires=["pytest", "requests", "psycopg2-binary"],
+    packages=find_packages(),
+)
